6021db83d1e3c6bfff9d08478e84c9cec4d9df25
Rishit Pushpkar <rishit.02pushpkar@gmail.com>
2023-05-16 15:57:09 +0530

    experiment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import HeaderSection from "./components/HeaderSection";
 import CardContainer from "./components/CardContainer";
-import Card from "./components/Card";
 
 const App = () => {
   return (
     <>
       <HeaderSection />
       <CardContainer />
-      <Card />
     </>
   );
 };
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,11 +3,13 @@ import styles from "./Card.module.css";
 const Card = () => {
   return (
     <>
-      <div className="card">
-        <input type="checkbox" />
-        <div className="todo-text"></div>
-        <i class="fa-regular fa-trash"></i>
-        <i></i>
+      <div className={styles.card}>
+        <input type="checkbox" className={styles.checkbox} />
+        <div className={styles.todo_text}>Todo</div>
+        <i
+          className={`${styles.trashIcon} fa-regular fa-trash-can`}
+          style={{ color: "#dc3545" }}
+        ></i>
       </div>
     </>
   );
